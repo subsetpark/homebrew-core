8bd827a00316b32afd4cf23195ac6266ebffdb59
Zach Smith <zd@zdsmith.com>
2016-07-15 12:36:22 -0400

    es 0.9.1

--- Formula/es.rb
+++ Formula/es.rb
@@ -1,8 +1,8 @@
 class Es < Formula
   desc "Extensible shell with first class functions, lexical scoping, and more"
   homepage "https://wryun.github.io/es-shell/"
-  url "https://github.com/downloads/wryun/es-shell/es-0.9.tar.gz"
-  sha256 "c4ab446642284924449c3d0a90c678fa7891aacd429ffdb3744899c44a298cfb"
+  url "https://github.com/wryun/es-shell/releases/download/v0.9.1/es-0.9.1.tar.gz"
+  sha256 "b0b41fce99b122a173a06b899a4d92e5bd3cc48b227b2736159f596a58fff4ba"
 
   bottle do
     cellar :any_skip_relocation
@@ -14,6 +14,7 @@ class Es < Formula
   option "with-readline", "Use readline instead of libedit"
 
   depends_on "readline" => :optional
+  depends_on "readline" => :linked
 
   conflicts_with "kes", :because => "both install 'es' binary"
 
